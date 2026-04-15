948c478b28197952f46906f7f473e79c0b4db26d
rangeshrmr2023-oss <YOUR_GITHUB_EMAIL>
2026-04-15 18:42:19 +0530

    payroll system

---
+++ src/main/java/com/example/payroll/App.java
@@ -0,0 +1,13 @@
+package com.example.payroll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
